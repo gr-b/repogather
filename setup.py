57201d8443aa1955d0a23b72a930934ad0b660a4
gr-b <griffin.bishop24@gmail.com>
2024-09-08 17:11:57 -0400

    Change setup version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='repogather',
-    version='0.0.2',
+    version='0.0.3',
     description="Easily copy all relevant source files in a repository to clipboard",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
