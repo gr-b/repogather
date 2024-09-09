28c36056c1060c52773562e1efface1234ec3026
gr-b <griffin.bishop24@gmail.com>
2024-09-09 18:46:16 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='repogather',
-    version='0.0.3',
+    version='0.0.4',
     description="Easily copy all relevant source files in a repository to clipboard",
     long_description=open('README.md').read(),
     long_description_content_type="text/markdown",
